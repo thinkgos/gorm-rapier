80c9e6f73084ca936b5f8b891efe99e8d8f5fef3
thinkgos <thinkgo@aliyun.com>
2023-04-26 08:10:38 +0800

    feat: add AsWithPrefix for field

--- expr_keyword.go
+++ expr_keyword.go
@@ -12,6 +12,17 @@ func (e expr) As(alias string) Expr {
 	return e
 }
 
+// AsWithPrefix use expr AS {prefix}_{ColumnName}
+func (e expr) AsWithPrefix(prefix string) Expr {
+	alias := prefix + "_" + e.col.Name
+	if e.e != nil {
+		e.e = clause.Expr{SQL: "? AS ?", Vars: []any{e.e, clause.Column{Name: alias}}}
+		return e
+	}
+	e.col.Alias = alias
+	return e
+}
+
 // Desc use expr DESC
 func (e expr) Desc() Expr {
 	e.e = clause.Expr{SQL: "? DESC", Vars: []any{e.RawExpr()}}
