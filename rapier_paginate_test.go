3c3c0a80ed1c59194b17a8030898f1a6607e4560
thinkgos <thinkgo@aliyun.com>
2024-11-21 10:26:32 +0800

    test: test limit

--- rapier_paginate_test.go
+++ rapier_paginate_test.go
@@ -52,7 +52,7 @@ func Test_Paginate(t *testing.T) {
 		}
 	})
 
-	t.Run("Paginate", func(t *testing.T) {
+	t.Run("Pagination", func(t *testing.T) {
 		tests := []struct {
 			name     string
 			db       *gorm.DB
@@ -82,6 +82,38 @@ func Test_Paginate(t *testing.T) {
 				db:       newDb().Model(Dict{}).Scopes(Pagination(1, DefaultMaxPerPage+1)).Find([]Dict{}),
 				wantVars: []any{500},
 				want:     "SELECT * FROM `dict` LIMIT ?",
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				ReviewBuildDb(t, tt.db, tt.want, tt.wantVars)
+			})
+		}
+	})
+	t.Run("Limit", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			db       *gorm.DB
+			wantVars []any
+			want     string
+		}{
+			{
+				name:     "in range",
+				db:       newDb().Model(Dict{}).Scopes(Limit(2, 10)).Find([]Dict{}),
+				wantVars: []any{10, 10},
+				want:     "SELECT * FROM `dict` LIMIT ? OFFSET ?",
+			},
+			{
+				name:     "page < 1",
+				db:       newDb().Model(Dict{}).Scopes(Limit(0, 10)).Find([]Dict{}),
+				wantVars: []any{10},
+				want:     "SELECT * FROM `dict` LIMIT ?",
+			},
+			{
+				name:     "perPage < 1",
+				db:       newDb().Model(Dict{}).Scopes(Limit(1, 0)).Find([]Dict{}),
+				wantVars: nil,
+				want:     "SELECT * FROM `dict`",
 			},
 		}
 		for _, tt := range tests {
